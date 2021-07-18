115ae21a591e40f1eda5bfa18096fabdc14c5bb4
Andreas Thomas <dev@chronark.com>
2021-07-18 09:33:01 +0200

    feat: add jwt middleware

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Security
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
+from typing import Dict, Any
+
+import jwt
+
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
 
 app = FastAPI()
 
+
+JWT_SIGNING_KEY = "secret"
+
+
+def jwt_middleware(auth: HTTPAuthorizationCredentials = Security(HTTPBearer())):
+    try:
+        return jwt.decode(auth.credentials, JWT_SIGNING_KEY, algorithms=["HS256"])
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Signature has expired")
+    except jwt.InvalidTokenError as e:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
 
 class Report(BaseModel):
     content: str
 
 
-
-
-
 @app.post("/classify")
-def classify_report(report: Report):
-    return {"Mach deinen scheiß selber": report}
+def classify_report(report: Report, claims: Dict[str, Any] = Depends(jwt_middleware)):
+    return {"Mach deinen scheiß selber": report, "claims": claims}
